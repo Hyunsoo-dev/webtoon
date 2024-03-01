c18f20bfda736046455e24fe1ff636fc272bc8ea
Hyunsoo-dev <jing07161@gmail.com>
2024-03-01 17:28:38 +0900

    Feature: App bar 구현

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -7,19 +7,34 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      appBar: AppBar(
-        elevation: 2,
-        title: const Center(
-          child: Text(
-            "Today's 툰s",
-            style: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.w500,
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(kToolbarHeight),
+        child: Container(
+          decoration: BoxDecoration(
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.5),
+                spreadRadius: 1,
+                blurRadius: 10, // 그림자 흐림 정도
+                offset: const Offset(0, 2), // 그림자 위치 조정
+              ),
+            ],
+          ),
+          child: AppBar(
+            elevation: 0.0,
+            title: const Center(
+              child: Text(
+                "Today's 툰s",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
             ),
+            foregroundColor: Colors.green,
+            backgroundColor: Colors.white,
           ),
         ),
-        foregroundColor: Colors.green,
-        backgroundColor: Colors.white,
       ),
     );
   }
